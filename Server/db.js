140fa7aba6fc39a38e192cfcc1e62e159e1ff99b
GeethaaSrinivasan <89134180+GeethaaSrinivasan@users.noreply.github.com>
2023-11-03 13:20:14 +0530

    Update db.js
    changed db details

--- Server/db.js
+++ Server/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql')
 const db = mysql.createConnection({
 host: "sql12.freesqldatabase.com",
-user: "sql12656888",
-password: "tIRbec3H7z",
-database:"sql12656888" 
+user: "sql12658777",
+password: "y7Vrcn4QaZ",
+database: "sql12658777" 
 })
 
 module.exports = db;
